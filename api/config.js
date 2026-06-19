dffae33d1319adb1ac03a84ec8c7623d1ef1a789
sovitprasad98-tech <sovitprasad98@gmail.com>
2026-06-19 12:51:49 +0530

    Update config.js

--- api/config.js
+++ api/config.js
@@ -21,14 +21,13 @@ module.exports = async (req, res) => {
 
   return res.status(200).json({
     firebase: {
-      apiKey:            process.env.FIREBASE_API_KEY,
-      authDomain:        process.env.FIREBASE_AUTH_DOMAIN,
-      databaseURL:       process.env.FIREBASE_DATABASE_URL,
-      projectId:         process.env.FIREBASE_PROJECT_ID,
-      storageBucket:     process.env.FIREBASE_STORAGE_BUCKET,
-      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-      appId:             process.env.FIREBASE_APP_ID,
-      measurementId:     process.env.FIREBASE_MEASUREMENT_ID,
+      apiKey:            process.env.AIzaSyBMY_dmCX2dDfES5ecDM57j4rrrv8_pPZc,
+      authDomain:        process.env.codexhub-85d35.firebaseapp.com,
+      databaseURL:       process.env.https://codexhub-85d35-default-rtdb.firebaseio.com,
+      projectId:         process.env.codexhub-85d35,
+      storageBucket:     process.env.codexhub-85d35.firebasestorage.app,
+      messagingSenderId: process.env.50514494667,
+      appId:             process.env.1:50514494667:web:facf9362e4082f867a474e,
     },
     zapKey: process.env.ZAP_KEY,
   });
